00b7102928042822f328583aa8f9d97e17f3885f
Siboniso Nene <sibonisodev@gmail.com>
2024-10-17 03:48:01 +0200

    Change python file name

---
+++ 2434209.py
@@ -0,0 +1,43 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data-file', type=str, default='Data/easy.txt',
+                    help='Process data input into the student and marker file')
+
+args = parser.parse_args()
+
+
+def main():
+
+
+    # Open the file for reading
+    try:
+        with open(args.data_file, "r") as file:
+            data = file.read()
+            file.close()
+
+    except FileNotFoundError:
+        return 1
+
+
+
+
+    """
+    TODO Your Algorithm - assign your output to the output variable
+    """
+
+
+
+
+    output = "AB AC AB !AD BA CB !BL BX AB" #Example output
+    
+    """
+    End of your algorithm
+    """
+
+    return output
+    
+
+if __name__ == "__main__":
+    scheduler_out = main()
+    print(scheduler_out)
